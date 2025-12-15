0f87849c08def6ae73e0d97e6131833b31813d18
madhavjadhavofficial <madhavjadhav.official@gmail.com>
2025-12-15 20:44:51 +0530

    assignment

---
+++ assignment/day 2/assign2q5.py
@@ -0,0 +1,14 @@
+def print_binary(n):
+    if n == 0:
+        print(0)
+        return
+    
+    binary = ""
+    while n > 0:
+        binary = str(n % 2) + binary
+        n = n // 2
+    
+    print("Binary format:", binary)
+
+num = int(input("Enter a number: "))
+print_binary(num)
